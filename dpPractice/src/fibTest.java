e9bb2f928feecfb19b5450da188dbde732309aa0
feixuanyu <994085848@qq.com>
2022-12-18 21:14:13 +0800

    509. 斐波那契数

---
+++ dpPractice/src/fibTest.java
@@ -0,0 +1,27 @@
+public class fibTest {
+
+
+    public static void main(String[] args) {
+
+        int res = fib(3);
+
+        System.out.println(res);
+    }
+
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+}
