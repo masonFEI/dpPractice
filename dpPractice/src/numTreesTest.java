b68d146e128c3b73c9b583163a840374b04a3ff5
feixuanyu <994085848@qq.com>
2022-12-21 23:33:05 +0800

    96. 不同的二叉搜索树

---
+++ dpPractice/src/numTreesTest.java
@@ -0,0 +1,35 @@
+public class numTreesTest {
+
+
+    public static void main(String[] args) {
+        int res = numTrees(3);
+        System.out.println(res);
+    }
+
+    public static int numTrees(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        if (n > 2) {
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+
+}
