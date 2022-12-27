c2c4a973915d579e7029630e2acd3c4c9546aeeb
masonFei <994085848@qq.com>
2022-12-27 21:25:08 +0800

    494. 目标和

--- dpPractice/src/findTargetSumWaysTest.java
+++ dpPractice/src/findTargetSumWaysTest.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class findTargetSumWaysTest {
 
 
@@ -5,6 +7,33 @@ public class findTargetSumWaysTest {
 
     }
 
+
+    public int findTargetSumWays(int[] nums, int target) {
+
+        int sum = Arrays.stream(nums).sum();
+
+        if (Math.abs(target) > sum || (target + sum) % 2 != 0) {
+            return 0;
+        }
+
+        int size = (target + sum) / 2;
+
+        if (size < 0) {
+            size = -size;
+        }
+
+        int[] dp = new int[size + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = size; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+
+        return dp[size];
+
+    }
 
 
 }
